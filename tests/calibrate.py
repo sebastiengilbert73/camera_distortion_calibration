54af699ece8b613622f6f76160f67532386ec329
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-11-27 15:44:10 -0500

    tests/calibrate.py: Group intersection points by horizontal and vertical lines

--- tests/calibrate.py
+++ tests/calibrate.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 import utilities.blob_analysis as blob_analysis
 import camera_distortion_calibration.checkerboard as checkerboard
+import camera_distortion_calibration.radial_distortion as radial_dist
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s \t%(message)s')
 
@@ -32,6 +33,23 @@ def main(
     )
     intersections_list = checkerboard_intersections.FindIntersections(checkerboard_img)
 
+    radial_distortion = radial_dist.RadialDistortion()
+    horizontal_lines_points, vertical_lines_points = radial_distortion.GroupCheckerboardPoints(intersections_list, (6, 6))
+
+    for line_ndx in range(len(horizontal_lines_points)):
+        horizontal_line_points = horizontal_lines_points[line_ndx]
+        color = ((177 * line_ndx)%256, (665 * line_ndx)%256, (735 * line_ndx)%256)
+        for p in horizontal_line_points:
+            cv2.circle(annotated_img, (round(p[0]), round(p[1])), 3, color)
+
+    for line_ndx in range(len(vertical_lines_points)):
+        vertical_line_points = vertical_lines_points[line_ndx]
+        color = ((641 * line_ndx)%256, (873 * line_ndx)%256, (489 * line_ndx)%256)
+        for p in vertical_line_points:
+            cv2.circle(annotated_img, (round(p[0]), round(p[1])), 7, color)
+
+    annotated_img_filepath = os.path.join(outputDirectory, "calibrate_main_annotated.png")
+    cv2.imwrite(annotated_img_filepath, annotated_img)
 
 
 if __name__ == '__main__':
