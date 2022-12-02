d842e0218dd36a444301be1b68a092ff1df57ad7
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-12-02 11:21:26 -0500

    tests/test_optimization.py: 1st commit

---
+++ tests/test_optimization.py
@@ -0,0 +1,29 @@
+import logging
+import os
+import camera_distortion_calibration.radial_distortion as radial_dist
+import torch
+import random
+import math
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s \t%(message)s')
+
+def main():
+    logging.debug("test_optimization.main()")
+
+    optimizer = radial_dist.DistortionParametersOptimizer((100, 95), 0)
+    real_rho = 89
+    real_theta = 2.06
+    noise_sigma = 3.0
+    points_tsr = torch.zeros(20, 2)
+    for pt_ndx in range(points_tsr.shape[0]):
+        beta = random.randint(-200, 200)
+        x = real_rho * math.cos(real_theta) + beta * math.sin(real_theta) + random.gauss(0, noise_sigma)
+        y = real_rho * math.sin(real_theta) - beta * math.cos(real_theta) + random.gauss(0, noise_sigma)
+        points_tsr[pt_ndx, 0] = x
+        points_tsr[pt_ndx, 1] = y
+    line = optimizer.Line(points_tsr)
+    logging.info(f"test_optimization.main(): line = {line}")
+
+
+if __name__ == '__main__':
+    main()
