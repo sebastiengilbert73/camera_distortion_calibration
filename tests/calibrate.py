4e01144eef0a32544595f59da03011a9a4ab22ca
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-12-03 08:57:07 -0500

    tests/calibrate.py: Calls radial_distortion.Optimize()

--- tests/calibrate.py
+++ tests/calibrate.py
@@ -1,3 +1,4 @@
+import ast
 import copy
 import os
 import logging
@@ -15,7 +16,8 @@ def main(
     outputDirectory,
     adaptiveThresholdBlockSide,
     adaptiveThresholdBias,
-    correlationThreshold
+    correlationThreshold,
+    gridShapeHW
 ):
     logging.info("calibrate.main()")
 
@@ -35,6 +37,11 @@ def main(
 
     radial_distortion = radial_dist.RadialDistortion()
     horizontal_lines_points, vertical_lines_points = radial_distortion.GroupCheckerboardPoints(intersections_list, (6, 6))
+    if len(horizontal_lines_points) != gridShapeHW[0]:
+        raise ValueError(f"calibrate.main(): len(horizontal_lines_points) ({len(horizontal_lines_points)}) != gridShapeHW[0] ({gridShapeHW[0]})")
+    if len(vertical_lines_points) != gridShapeHW[1]:
+        raise ValueError(f"calibrate.main(): len(vertical_lines_points) ({len(vertical_lines_points)}) != gridShapeHW[1] ({gridShapeHW[1]})")
+
 
     for line_ndx in range(len(horizontal_lines_points)):
         horizontal_line_points = horizontal_lines_points[line_ndx]
@@ -48,6 +55,32 @@ def main(
         for p in vertical_line_points:
             cv2.circle(annotated_img, (round(p[0]), round(p[1])), 7, color)
 
+    epoch_loss_center_alpha_list = radial_distortion.Optimize(intersections_list, gridShapeHW, (checkerboard_img.shape[0], checkerboard_img.shape[1]))
+    logging.info(f"radial_distortion.center = {radial_distortion.center}; radial_distortion.alpha = {radial_distortion.alpha}")
+    #print(f"epoch_loss_center_alpha_list =\n{epoch_loss_center_alpha_list}")
+
+    # Save the points as an (N, n_points, 2) tensor
+    horizontal_intersections_arr = np.zeros((len(horizontal_lines_points), len(horizontal_lines_points[0]), 2), dtype=float)
+    for line_ndx in range(len(horizontal_lines_points)):
+        points_list = horizontal_lines_points[line_ndx]
+        for point_ndx in range(len(points_list)):
+            xy = points_list[point_ndx]
+            horizontal_intersections_arr[line_ndx, point_ndx, 0] = xy[0]
+            horizontal_intersections_arr[line_ndx, point_ndx, 1] = xy[1]
+    horizontal_intersections_arr_filepath = os.path.join(outputDirectory, "calibrate_main_horizontalIntersections.npy")
+    np.save(horizontal_intersections_arr_filepath, horizontal_intersections_arr)
+
+    vertical_intersections_arr = np.zeros((len(vertical_lines_points), len(vertical_lines_points[0]), 2),
+                                            dtype=float)
+    for line_ndx in range(len(vertical_lines_points)):
+        points_list = vertical_lines_points[line_ndx]
+        for point_ndx in range(len(points_list)):
+            xy = points_list[point_ndx]
+            vertical_intersections_arr[line_ndx, point_ndx, 0] = xy[0]
+            vertical_intersections_arr[line_ndx, point_ndx, 1] = xy[1]
+    vertical_intersections_arr_filepath = os.path.join(outputDirectory, "calibrate_main_verticalIntersections.npy")
+    np.save(vertical_intersections_arr_filepath, vertical_intersections_arr)
+
     annotated_img_filepath = os.path.join(outputDirectory, "calibrate_main_annotated.png")
     cv2.imwrite(annotated_img_filepath, annotated_img)
 
@@ -60,11 +93,15 @@ if __name__ == '__main__':
     parser.add_argument('--adaptiveThresholdBlockSide', help="For adaptive threshold, the side of the neighborhood. Default: 17", type=int, default=17)
     parser.add_argument('--adaptiveThresholdBias', help="For adaptive threshold, the bias C. Default: -10", type=int, default=-10)
     parser.add_argument('--correlationThreshold', help="The correlation threshold. Default: 0.8", type=float, default=0.8)
+    parser.add_argument('--gridShapeHW', help="The shape of the intersections grid (height, width). Default: '(6, 6)'", default='(6, 6)')
     args = parser.parse_args()
+
+    gridShapeHW = ast.literal_eval(args.gridShapeHW)
     main(
         args.imageFilepath,
         args.outputDirectory,
         args.adaptiveThresholdBlockSide,
         args.adaptiveThresholdBias,
-        args.correlationThreshold
+        args.correlationThreshold,
+        gridShapeHW
     )
